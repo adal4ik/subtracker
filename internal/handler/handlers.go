1ba9cfac61f3e3c600ba7c630189970d5ee16170
adal4ik <adilet.adilek@gmail.com>
2025-07-15 21:36:13 +0500

    feat: initial project structure

---
+++ internal/handler/handlers.go
@@ -0,0 +1,11 @@
+package handler
+
+type Handlers struct {
+	// Add fields and methods for your handler here
+}
+
+func NewHandlers() *Handlers {
+	return &Handlers{
+		// Initialize your handler fields here
+	}
+}
