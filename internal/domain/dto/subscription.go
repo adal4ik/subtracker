969e4860769226f9c41c474d40443261d2973b38
adal4ik <adilet.adilek@gmail.com>
2025-07-18 15:33:42 +0500

    fix(dto): uuid fixed

--- internal/domain/dto/subscription.go
+++ internal/domain/dto/subscription.go
@@ -16,3 +16,15 @@ type SubscriptionResponse struct {
 	StartDate   string `json:"start_date"`
 	EndDate     string `json:"end_date,omitempty"`
 }
+
+type SubscriptionFilter struct {
+	UserID      string  `json:"user_id"`
+	ServiceName string  `json:"service_name"`
+	MinPrice    float64 `json:"min_price"`
+	MaxPrice    float64 `json:"max_price"`
+	StartDate   string  `json:"start_date"`
+	EndDate     string  `json:"end_date"`
+	HasEndDate  *bool   `json:"has_end_date"`
+	Limit       int     `json:"limit"`
+	Offset      int     `json:"offset"`
+}
